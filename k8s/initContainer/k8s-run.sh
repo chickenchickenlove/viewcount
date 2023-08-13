a46ea740379a11a6e300e7ff753244ae66d95efa
chickenchickenlove <ojt90902@naver.com>
2023-08-13 14:13:41 +0900

    add initcontainer and manifest

--- k8s/initContainer/k8s-run.sh
+++ k8s/initContainer/k8s-run.sh
@@ -3,7 +3,12 @@
 SVC=$1
 PORT=$2
 
+
+echo "SVC=$SVC, PORT=$PORT"
 echo "this script run."
 
 mysql --host "$SVC" --port "$PORT" -u root -p1234 viewcount-db < /db-init.sql
 sleep 2
+
+
+# docker buildx build --platform linux/arm64 --push -t ojt90902/viewcount-test:latest .
